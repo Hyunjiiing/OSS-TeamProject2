31cb22fe73b1218c2bb5daa54e987363b5653266
KangJiUng <woong236406@gmail.com>
2023-04-30 21:24:05 +0900

    add meal records' UI Design

--- lib/J_mealrecordUI.dart
+++ lib/J_mealrecordUI.dart
@@ -15,11 +15,29 @@ class MyApp extends StatelessWidget {
             children: [
               Text('오늘의 식단 기록하기'),
               Text('0000년 00월 00일'),
-              Text('아침: '),
+              TextButton(
+                  style: ButtonStyle(
+                      foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                      backgroundColor: MaterialStateProperty.all(Colors.grey)),
+                  onPressed: () {},
+                  child: Text('아침'),),
+                Text('칼로리: '),
+                TextButton(
+                  style: ButtonStyle(
+                      foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                      backgroundColor: MaterialStateProperty.all(Colors.grey)),
+                  onPressed: () {},
+                  child: Text('점심'),),
               Text('칼로리: '),
-              Text('점심: '),
-              Text('칼로리: '),
-              Text('저녁: '),
+              TextButton(
+                style: ButtonStyle(
+                    foregroundColor:
+                    MaterialStateProperty.all<Color>(Colors.black),
+                    backgroundColor: MaterialStateProperty.all(Colors.grey)),
+                onPressed: () {},
+                child: Text('저녁'),),
               Text('칼로리: '),
             ],
           )
