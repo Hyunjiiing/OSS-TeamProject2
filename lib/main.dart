2a1a79ac2bce145fbf0db179d06908839b387ef2
Hyunjiiing <h.jilluminate@gmail.com>
2023-04-30 18:51:32 +0900

    feat: mainpage user information

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home:MyApp()),);
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  final String foxImageUrl = "https://cdn.pixabay.com/photo/2018/07/30/04/23/fox-illustration-3571760_960_720.png";
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white70,
           title: Text(
@@ -21,7 +22,17 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+              Image.network(foxImageUrl ,width: 100,
+                height: 150,),
+          Text("이름 : 여우입니다"),
+          Text("나이 : 26살"),
+          Text("몸무게 : 55kg"),
+          Text("몸무게 : 55kg"),
+        ],
       ),
-    );
+      );
   }
 }
