e4e00e7860fe489e2744dbf9c5052b3929bfabe6
LSA31 <tmddk811@naver.com>
2023-05-29 18:57:31 +0900

    캘린더뷰

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -43,55 +43,61 @@ super.dispose();
 
 @override
 Widget build(BuildContext context) {
-return Scaffold(
-body: Column(
-children: [
-TableCalendar(
-locale: 'ko_KR',
-firstDay: DateTime.utc(2021, 10, 16),
-lastDay: DateTime.utc(2030, 3, 14),
-focusedDay: focusedDay,
-selectedDayPredicate: (DateTime day) {
-return isSameDay(selectedDay, day);
-},
-onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-setState(() {
-this.selectedDay = selectedDay;
-this.focusedDay = focusedDay;
-memoController.text = events[selectedDay]?.join('\n') ?? '';
-});
-},
-eventLoader: (day) {
-return events[day] ?? [];
-},
-calendarBuilders: CalendarBuilders(
-markerBuilder: (context, day, events) {
-if (events.isNotEmpty) {
-return Container(
-width: 6,
-height: 6,
-decoration: BoxDecoration(
-shape: BoxShape.circle,
-color: const Color(0xFFFF923F),
-),
-);
-}
-return null;
-},
-selectedBuilder: (context, day, events) {
-return Container(
-decoration: BoxDecoration(
-shape: BoxShape.circle,
-color: const Color(0xFFFF923F),
-),
-margin: const EdgeInsets.all(8),
-alignment: Alignment.center,
-child: Text(
-day.day.toString(),
-style: TextStyle(
-color: Colors.white,
-fontWeight: FontWeight.bold,
-),
-),
-);
-},
+  return Scaffold(
+      body: Column(
+      children: [
+      TableCalendar(
+      locale: 'ko_KR',
+      firstDay: DateTime.utc(2021, 10, 16),
+  lastDay: DateTime.utc(2030, 3, 14),
+  focusedDay: focusedDay,
+  selectedDayPredicate: (DateTime day) {
+  return isSameDay(selectedDay, day);
+  },
+  onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+  setState(() {
+  this.selectedDay = selectedDay;
+  this.focusedDay = focusedDay;
+  memoController.text = events[selectedDay]?.join('\n') ?? '';
+  });
+  },
+  eventLoader: (day) {
+  return events[day] ?? [];
+  },
+  calendarBuilders: CalendarBuilders(
+  markerBuilder: (context, day, events) {
+  if (events.isNotEmpty) {
+  return Container(
+  width: 6,
+  height: 6,
+  decoration: BoxDecoration(
+  shape: BoxShape.circle,
+  color: const Color(0xFFFF923F),
+  ),
+  );
+  }
+  return null;
+  },
+  selectedBuilder: (context, day, events) {
+  return Container(
+  decoration: BoxDecoration(
+  shape: BoxShape.circle,
+  color: const Color(0xFFFF923F),
+  ),
+  margin: const EdgeInsets.all(8),
+  alignment: Alignment.center,
+  child: Text(
+  day.day.toString(),
+  style: TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  ),
+  ),
+  );
+  },
+  todayBuilder: (context, day, events) {
+  return Container(
+  decoration: BoxDecoration(
+  shape: BoxShape.circle,
+  color: const Color(0xFFFF923F),
+  ),
