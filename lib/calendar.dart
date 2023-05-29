78de40681510d827828340c29914ee8cdf040ea7
LSA31 <tmddk811@naver.com>
2023-05-29 18:57:58 +0900

    캘린더뷰

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -101,3 +101,16 @@ Widget build(BuildContext context) {
   shape: BoxShape.circle,
   color: const Color(0xFFFF923F),
   ),
+  margin: const EdgeInsets.all(8),
+  alignment: Alignment.center,
+  child: Text(
+  day.day.toString(),
+  style: TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  ),
+  ),
+  );
+  },
+  ),
+  ),
