12d90fc4eb810e0ad849f09a39a4714e1b873791
LSA31 <tmddk811@naver.com>
2023-05-29 18:56:59 +0900

    캘린더뷰

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,54 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() {
+initializeDateFormatting();
+runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
 @override
 Widget build(BuildContext context) {
-  return Scaffold(
-      body: Column(
-      children: [
-      TableCalendar(
-      locale: 'ko_KR',
-      firstDay: DateTime.utc(2021, 10, 16),
-  lastDay: DateTime.utc(2030, 3, 14),
-  focusedDay: focusedDay,
-  selectedDayPredicate: (DateTime day) {
-  return isSameDay(selectedDay, day);
-  },
-  onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-  setState(() {
-  this.selectedDay = selectedDay;
-  this.focusedDay = focusedDay;
-  memoController.text = events[selectedDay]?.join('\n') ?? '';
-  });
-  },
-  eventLoader: (day) {
-  return events[day] ?? [];
-  },
-  calendarBuilders: CalendarBuilders(
-  markerBuilder: (context, day, events) {
-  if (events.isNotEmpty) {
-  return Container(
-  width: 6,
-  height: 6,
-  decoration: BoxDecoration(
-  shape: BoxShape.circle,
-  color: const Color(0xFFFF923F),
-  ),
-  );
-  }
-  return null;
-  },
-  selectedBuilder: (context, day, events) {
-  return Container(
-  decoration: BoxDecoration(
-  shape: BoxShape.circle,
-  color: const Color(0xFFFF923F),
-  ),
-  margin: const EdgeInsets.all(8),
-  alignment: Alignment.center,
-  child: Text(
-  day.day.toString(),
-  style: TextStyle(
-  color: Colors.white,
-  fontWeight: FontWeight.bold,
-  ),
-  ),
-  );
-  },
+return MaterialApp(
+home: TableCalendarScreen(),
+);
+}
+}
+
+class TableCalendarScreen extends StatefulWidget {
+const TableCalendarScreen({Key? key}) : super(key: key);
+
+@override
+State<TableCalendarScreen> createState() => _TableCalendarScreenState();
+}
+
+class _TableCalendarScreenState extends State<TableCalendarScreen> {
+DateTime selectedDay = DateTime(
+DateTime.now().year,
+DateTime.now().month,
+DateTime.now().day,
+);
+
+DateTime focusedDay = DateTime.now();
+Map<DateTime, List<String>> events = {};
+
+TextEditingController memoController = TextEditingController();
+
+@override
+void dispose() {
+memoController.dispose();
+super.dispose();
+}
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+body: Column(
+children: [
+TableCalendar(
+locale: 'ko_KR',
+firstDay: DateTime.utc(2021, 10, 16),
+lastDay: DateTime.utc(2030, 3, 14),
+focusedDay: focusedDay,
+selectedDayPredicate: (DateTime day) {
+return isSameDay(selectedDay, day);
+},
+onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+setState(() {
+this.selectedDay = selectedDay;
+this.focusedDay = focusedDay;
+memoController.text = events[selectedDay]?.join('\n') ?? '';
+});
+},
+eventLoader: (day) {
+return events[day] ?? [];
+},
+calendarBuilders: CalendarBuilders(
+markerBuilder: (context, day, events) {
+if (events.isNotEmpty) {
+return Container(
+width: 6,
+height: 6,
+decoration: BoxDecoration(
+shape: BoxShape.circle,
+color: const Color(0xFFFF923F),
+),
+);
+}
+return null;
+},
+selectedBuilder: (context, day, events) {
+return Container(
+decoration: BoxDecoration(
+shape: BoxShape.circle,
+color: const Color(0xFFFF923F),
+),
+margin: const EdgeInsets.all(8),
+alignment: Alignment.center,
+child: Text(
+day.day.toString(),
+style: TextStyle(
+color: Colors.white,
+fontWeight: FontWeight.bold,
+),
+),
+);
+},
