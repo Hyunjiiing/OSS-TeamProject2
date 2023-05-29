1e08660f87569831e06ecd1675c510c0af488c3e
LSA31 <tmddk811@naver.com>
2023-05-29 22:27:04 +0900

    캘린더뷰 expended

--- app/lib/calview.dart
+++ app/lib/calview.dart
@@ -61,69 +61,78 @@ class _TableCalendarScreenState extends State<TableCalendarScreen> {
                 memoController.text = events[selectedDay]?.join('\n') ?? '';
               });
             },
-        eventLoader: (day) {
-          return events[day] ?? [];
-        },
-        calendarBuilders: CalendarBuilders(
-            markerBuilder: (context, day, events) {
-              if (events.isNotEmpty) {
+            eventLoader: (day) {
+              return events[day] ?? [];
+            },
+            calendarBuilders: CalendarBuilders(
+              markerBuilder: (context, day, events) {
+                if (events.isNotEmpty) {
+                  return Container(
+                    width: 6,
+                    height: 6,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: const Color(0xFFFF923F),
+                    ),
+                  );
+                }
+                return null;
+              },
+              selectedBuilder: (context, day, events) {
                 return Container(
-                  width: 6,
-                  height: 6,
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     color: const Color(0xFFFF923F),
                   ),
+                  margin: const EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text(
+                    day.day.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 );
-              }
-              return null;
-            },
-    selectedBuilder: (context, day, events) {
-    return Container(
-    decoration: BoxDecoration(
-    shape: BoxShape.circle,
-    color: const Color(0xFFFF923F),
-    ),
-      margin: const EdgeInsets.all(8),
-      alignment: Alignment.center,
-      child: Text(
-        day.day.toString(),
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    );
-    },
-    todayBuilder: (context, day, events) {
-    return Container(
-    decoration: BoxDecoration(
-    shape: BoxShape.circle,
-    color: const Color(0xFFFF923F),
-    ),
-      margin: const EdgeInsets.all(8),
-      alignment: Alignment.center,
-      child: Text(
-        day.day.toString(),
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    );
-    },
-        ),
-      ),
-    Expanded(
-    child: Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: TextField(
-    controller: memoController,
-    decoration: InputDecoration(
-    hintText: '메모를 입력하세요',
-    border: OutlineInputBorder(),
-    ),
+              },
+              todayBuilder: (context, day, events) {
+                return Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: const Color(0xFFFF923F),
+                  ),
+                  margin: const EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  child: Text(
+                    day.day.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: memoController,
+                decoration: InputDecoration(
+                  hintText: '메모를 입력하세요',
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    events[selectedDay] = value.isNotEmpty ? value.split('\n') : [];
+                  });
+                },
+              ),
+            ),
+          ),
+          if (events[selectedDay]?.isNotEmpty == true)
+            Text('메모: ${events[selectedDay]?.join('\n')}'), // 선택한 날짜의 메모 표시
         ],
       ),
     );
