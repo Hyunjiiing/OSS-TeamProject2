1355e86925ddf9c252ffb3f7a32b10fedf029ea9
LSA31 <tmddk811@naver.com>
2023-05-29 18:59:04 +0900

    캘린더뷰

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -44,82 +44,97 @@ super.dispose();
 @override
 Widget build(BuildContext context) {
   return Scaffold(
-      body: Column(
+    body: Column(
       children: [
-      TableCalendar(
-      locale: 'ko_KR',
-      firstDay: DateTime.utc(2021, 10, 16),
-  lastDay: DateTime.utc(2030, 3, 14),
-  focusedDay: focusedDay,
-  selectedDayPredicate: (DateTime day) {
-  return isSameDay(selectedDay, day);
-  },
-  onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-  setState(() {
-  this.selectedDay = selectedDay;
-  this.focusedDay = focusedDay;
-  memoController.text = events[selectedDay]?.join('\n') ?? '';
-  });
-  },
-  eventLoader: (day) {
-  return events[day] ?? [];
-  },
-  calendarBuilders: CalendarBuilders(
-  markerBuilder: (context, day, events) {
-  if (events.isNotEmpty) {
-  return Container(
-  width: 6,
-  height: 6,
-  decoration: BoxDecoration(
-  shape: BoxShape.circle,
-  color: const Color(0xFFFF923F),
-  ),
+        TableCalendar(
+          locale: 'ko_KR',
+          firstDay: DateTime.utc(2021, 10, 16),
+          lastDay: DateTime.utc(2030, 3, 14),
+          focusedDay: focusedDay,
+          selectedDayPredicate: (DateTime day) {
+            return isSameDay(selectedDay, day);
+          },
+          onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+            setState(() {
+              this.selectedDay = selectedDay;
+              this.focusedDay = focusedDay;
+              memoController.text = events[selectedDay]?.join('\n') ?? '';
+            });
+          },
+          eventLoader: (day) {
+            return events[day] ?? [];
+          },
+          calendarBuilders: CalendarBuilders(
+            markerBuilder: (context, day, events) {
+              if (events.isNotEmpty) {
+                return Container(
+                  width: 6,
+                  height: 6,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: const Color(0xFFFF923F),
+                  ),
+                );
+              }
+              return null;
+            },
+            selectedBuilder: (context, day, events) {
+              return Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: const Color(0xFFFF923F),
+                ),
+                margin: const EdgeInsets.all(8),
+                alignment: Alignment.center,
+                child: Text(
+                  day.day.toString(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              );
+            },
+            todayBuilder: (context, day, events) {
+              return Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: const Color(0xFFFF923F),
+                ),
+                margin: const EdgeInsets.all(8),
+                alignment: Alignment.center,
+                child: Text(
+                  day.day.toString(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: memoController,
+              decoration: InputDecoration(
+                hintText: '메모를 입력하세요',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  events[selectedDay] = value.isNotEmpty ? value.split('\n') : [];
+                });
+              },
+            ),
+          ),
+        ),
+        if (events[selectedDay]?.isNotEmpty == true)
+          Text('메모: ${events[selectedDay]?.join('\n')}'), // 선택한 날짜의 메모 표시
+      ],
+    ),
   );
-  }
-  return null;
-  },
-  selectedBuilder: (context, day, events) {
-  return Container(
-  decoration: BoxDecoration(
-  shape: BoxShape.circle,
-  color: const Color(0xFFFF923F),
-  ),
-  margin: const EdgeInsets.all(8),
-  alignment: Alignment.center,
-  child: Text(
-  day.day.toString(),
-  style: TextStyle(
-  color: Colors.white,
-  fontWeight: FontWeight.bold,
-  ),
-  ),
-  );
-  },
-  todayBuilder: (context, day, events) {
-  return Container(
-  decoration: BoxDecoration(
-  shape: BoxShape.circle,
-  color: const Color(0xFFFF923F),
-  ),
-  margin: const EdgeInsets.all(8),
-  alignment: Alignment.center,
-  child: Text(
-  day.day.toString(),
-  style: TextStyle(
-  color: Colors.white,
-  fontWeight: FontWeight.bold,
-  ),
-  ),
-  );
-  },
-  ),
-  ),
-  Expanded(
-  child: Padding(
-  padding: const EdgeInsets.all(16.0),
-  child: TextField(
-  controller: memoController,
-  decoration: InputDecoration(
-  hintText: '메모를 입력하세요',
-  border: OutlineInputBorder(),
-  ),
+}
+}
