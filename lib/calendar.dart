4a8fbe31727123d7fd715fdc9ca60d673b35c155
LSA31 <tmddk811@naver.com>
2023-05-29 18:46:49 +0900

    캘린더뷰

---
+++ lib/calendar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() {
+  initializeDateFormatting();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TableCalendarScreen(),
+    );
+  }
+}
+
+class TableCalendarScreen extends StatefulWidget {
+  const TableCalendarScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TableCalendarScreen> createState() => _TableCalendarScreenState();
+}
