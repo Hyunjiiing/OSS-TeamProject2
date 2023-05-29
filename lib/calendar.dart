f16fae30ef5442e9c1339fdf9ca24700b3c412fe
LSA31 <tmddk811@naver.com>
2023-05-29 18:55:28 +0900

    캘린더뷰

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -76,3 +76,9 @@ class _TableCalendarScreenState extends State<TableCalendarScreen> {
     }
     return null;
     },
+    selectedBuilder: (context, day, events) {
+    return Container(
+    decoration: BoxDecoration(
+    shape: BoxShape.circle,
+    color: const Color(0xFFFF923F),
+    ),
