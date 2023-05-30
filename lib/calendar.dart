f8d857102018102b71cfe511ac481523b1d1d264
LSA31 <tmddk811@naver.com>
2023-05-30 18:26:26 +0900

    캘린더뷰 초기 세팅

---
+++ lib/calendar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() {
+  initializeDateFormatting();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TableCalendarScreen(),
+    );
+  }
+}
