4e4844cc527ac727f548a1f593b7a86a75773e85
LSA31 <tmddk811@naver.com>
2023-05-30 18:30:01 +0900

    캘린더뷰 TextEditingController

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -15,17 +15,25 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class TableCalendarScreen extends StatefulWidget {
   const TableCalendarScreen({Key? key}) : super(key: key);
 
   @override
   State<TableCalendarScreen> createState() => _TableCalendarScreenState();
 }
+
 class _TableCalendarScreenState extends State<TableCalendarScreen> {
   DateTime selectedDay = DateTime(
     DateTime.now().year,
     DateTime.now().month,
     DateTime.now().day,
   );
-DateTime focusedDay = DateTime.now();
-Map<DateTime, List<String>> events = {};
+
+  DateTime focusedDay = DateTime.now();
+  Map<DateTime, List<String>> events = {};
+
+  TextEditingController menstrualPeriodController = TextEditingController();
+  TextEditingController nutritionalSupplementsController = TextEditingController();
+  TextEditingController conditionController = TextEditingController();
+  TextEditingController waterIntakeController = TextEditingController();
