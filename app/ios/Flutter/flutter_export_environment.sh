48bb614058aef5f210fa67fad1ea2a7ae20f8c96
KangJiUng <woong236406@gmail.com>
2023-06-02 22:20:19 +0900

    reset commit

--- app/ios/Flutter/flutter_export_environment.sh
+++ app/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\SAMSUNG\OneDrive\바탕 화면\mealrecordui"
+export "FLUTTER_APPLICATION_PATH=C:\Users\SAMSUNG\OneDrive\바탕 화면\mealrecordui\app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
