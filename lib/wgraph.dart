e62048f88a71b97f0140f2f350cc4f9c8febda03
LSA31 <tmddk811@naver.com>
2023-06-05 14:15:09 +0900

    체중그래프 saveWeightEntry

--- lib/wgraph.dart
+++ lib/wgraph.dart
@@ -22,3 +22,17 @@ class WeightTrackerPage extends StatefulWidget {
   @override
   _WeightTrackerPageState createState() => _WeightTrackerPageState();
 }
+
+class _WeightTrackerPageState extends State<WeightTrackerPage> {
+  List<FlSpot> weightEntries = [];
+  TextEditingController weightController = TextEditingController();
+
+  void _saveWeightEntry() {
+    setState(() {
+      double? weight = double.tryParse(weightController.text);
+      if (weight != null) {
+        weightEntries.add(FlSpot(weightEntries.length.toDouble(), weight));
+        weightController.clear();
+      }
+    });
+  }
