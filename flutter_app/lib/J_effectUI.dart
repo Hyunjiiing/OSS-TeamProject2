24baf956089a54cfee372bf2a11e331d2ec5c353
KangJiUng <woong236406@gmail.com>
2023-06-07 02:10:46 +0900

    사용자 입력 UI 부분 수정

--- flutter_app/lib/J_effectUI.dart
+++ flutter_app/lib/J_effectUI.dart
@@ -35,8 +35,8 @@ class _DietCalculatorScreenState extends State<DietCalculatorScreen> {
   String _apiResponse = '';
   String _comparisonMessage = '';
 
-  final List<String> genderOptions = ['Male', 'Female'];
-  final List<String> activityLevelOptions = [
+  List<String> genderOptions = ['Male', 'Female'];
+  List<String> activityLevelOptions = [
     'Level 1: 운동을 거의 또는 전혀 하지 않음',
     'Level 2: 일주일에 1-3회 운동',
     'Level 3: 일주일에 4-5회 운동',
@@ -99,109 +99,139 @@ class _DietCalculatorScreenState extends State<DietCalculatorScreen> {
   }
 
   @override
+  void dispose() {
+    _ageController.dispose();
+    _heightController.dispose();
+    _weightController.dispose();
+    _calorieController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('다이어트를 위한 일일섭취칼로리량'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '나이',
-              style: TextStyle(fontSize: 18.0),
-            ),
-            TextFormField(
-              controller: _ageController,
-              keyboardType: TextInputType.number,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return '나이를 입력해주세요.';
-                }
-                return null;
-              },
-            ),
-            SizedBox(height: 16),
-            Text(
-              '성별',
-              style: TextStyle(fontSize: 18),
-            ),
-            DropdownButtonFormField<String>(
-              value: _gender,
-              onChanged: (value) {
-                setState(() {
-                  _gender = value;
-                });
-              },
-              items: genderOptions.map((String option) {
-                return DropdownMenuItem<String>(
-                  value: option,
-                  child: Text(option),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 16),
-            Text(
-              '키',
-              style: TextStyle(fontSize: 18),
-            ),
-            TextFormField(
-              onChanged: (value) {
-                setState(() {
-                  _height = value;
-                });
-              },
-            ),
-            SizedBox(height: 16),
-            Text(
-              '체중',
-              style: TextStyle(fontSize: 18),
-            ),
-            TextFormField(
-              onChanged: (value) {
-                setState(() {
-                  _weight = value;
-                });
-              },
-            ),
-            SizedBox(height: 16),
-            Text(
-              '활동 수준을 선택해주세요.',
-              style: TextStyle(fontSize: 18),
-            ),
-            DropdownButtonFormField<String>(
-              value: _activityLevel,
-              onChanged: (value) {
-                setState(() {
-                  _activityLevel = value;
-                });
-              },
-              items: activityLevelOptions.map((String option) {
-                return DropdownMenuItem<String>(
-                  value: option,
-                  child: Text(option),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _calculateDailyCalorie,
-              child: Text('일일 섭취 칼로리 구하기'),
-            ),
-            SizedBox(height: 16.0),
-            if (_apiResponse.isNotEmpty)
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
               Text(
-                '일일 섭취 칼로리량: $_apiResponse',
+                '나이',
                 style: TextStyle(fontSize: 18.0),
               ),
-            if (_comparisonMessage.isNotEmpty)
+              TextFormField(
+                controller: _ageController,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '나이를 입력해주세요.';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
               Text(
-                _comparisonMessage,
+                '성별',
                 style: TextStyle(fontSize: 18.0),
               ),
-          ],
+              DropdownButtonFormField(
+                value: _gender,
+                items: genderOptions.map((String option) {
+                  return DropdownMenuItem(
+                    value: option,
+                    child: Text(option),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    _gender = value!;
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '성별을 선택해주세요.';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                '키 (cm)',
+                style: TextStyle(fontSize: 18.0),
+              ),
+              TextFormField(
+                controller: _heightController,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '키를 입력해주세요.';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                '체중 (kg)',
+                style: TextStyle(fontSize: 18.0),
+              ),
+              TextFormField(
+                controller: _weightController,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '체중을 입력해주세요.';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                '활동 수준',
+                style: TextStyle(fontSize: 18.0),
+              ),
+              DropdownButtonFormField(
+                value: _activityLevel,
+                items: activityLevelOptions.map((String option) {
+                  return DropdownMenuItem(
+                    value: option,
+                    child: Text(option),
+                  );
+                }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    _activityLevel = value!;
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return '활동 수준을 선택해주세요.';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _calculateDailyCalorie,
+                child: Text('계산하기'),
+              ),
+              SizedBox(height: 16.0),
+              if (_apiResponse.isNotEmpty)
+                Text(
+                  '일일 섭취 칼로리량: $_apiResponse',
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              if (_comparisonMessage.isNotEmpty)
+                Text(
+                  _comparisonMessage,
+                  style: TextStyle(fontSize: 18.0),
+                ),
+            ],
+          ),
         ),
       ),
     );
