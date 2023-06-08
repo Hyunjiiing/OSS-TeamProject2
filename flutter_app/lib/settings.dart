3088dc2fa7dd8acbbb5d2ad6ad408bb7f26a4d00
Hyunjiiing <h.jilluminate@gmail.com>
2023-06-09 03:54:40 +0900

    설정화면 Text 내용 추가 및 코드 리포맷

--- flutter_app/lib/settings.dart
+++ flutter_app/lib/settings.dart
@@ -15,75 +15,75 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         primaryColor: Color(0xFFFF923F),
-        brightness: Provider
-            .of<Settings>(context)
-            .isDarkMode
+        brightness: Provider.of<Settings>(context).isDarkMode
             ? Brightness.dark
             : Brightness.light,
       ),
       home: Scaffold(
-        appBar: AppBar(title: Text('다이어트 설정')),
+        appBar: AppBar(title: Text('설정')),
         body: SettingScreen(),
       ),
     );
   }
 }
+
 class SettingScreen extends StatefulWidget {
-@override
-_SettingScreenState createState() => _SettingScreenState();
+  @override
+  _SettingScreenState createState() => _SettingScreenState();
 }
 
 class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
     return ListView(
-        children: [
-    ListTile(
-    title: Text('푸시 알림 설정'),
-    trailing: Consumer<Settings>(
-    builder: (context, settings, _) {
-    return Switch(
-    value: settings.notificationEnabled,
-    onChanged: (value) {
-    setState(() {
-    settings.notificationEnabled = value;
-    });
-    },
-    );
-    },
-    ),
-    ),
-    ListTile(
-    title: Text('테마'),
-    trailing: DropdownButton<ThemeMode>(
-    value: Provider.of<Settings>(context).themeMode,
-    items: [
-    DropdownMenuItem(child: Text('기본 테마'), value: ThemeMode.light),
-    DropdownMenuItem(child: Text('다크 모드'), value: ThemeMode.dark),
-    ],
-    onChanged: (value) {
-    setState(() {
-    Provider.of<Settings>(context, listen: false).themeMode = value!;
-    });
-    },
-    ),
-    ),
-    ListTile(
-    title: Text('언어 설정'),
-    trailing: DropdownButton<Language>(
-    value: Provider.of<Settings>(context).language,
-    items: [
-    DropdownMenuItem(child: Text('한국어'), value: Language.korean),
-      DropdownMenuItem(child: Text('영어'), value: Language.english),
-    ],
-      onChanged: (value) {
-        setState(() {
-          Provider.of<Settings>(context, listen: false).language = value!;
-        });
-      },
-    ),
-    ),
-        ],
+      children: [
+        ListTile(
+          title: Text('푸시 알림 설정 : ON/OFF'),
+          trailing: Consumer<Settings>(
+            builder: (context, settings, _) {
+              return Switch(
+                value: settings.notificationEnabled,
+                onChanged: (value) {
+                  setState(() {
+                    settings.notificationEnabled = value;
+                  });
+                },
+              );
+            },
+          ),
+        ),
+        ListTile(
+          title: Text('테마 설정'),
+          trailing: DropdownButton<ThemeMode>(
+            value: Provider.of<Settings>(context).themeMode,
+            items: [
+              DropdownMenuItem(child: Text('기본 테마'), value: ThemeMode.light),
+              DropdownMenuItem(child: Text('다크 테마'), value: ThemeMode.dark),
+            ],
+            onChanged: (value) {
+              setState(() {
+                Provider.of<Settings>(context, listen: false).themeMode =
+                    value!;
+              });
+            },
+          ),
+        ),
+        ListTile(
+          title: Text('언어 설정'),
+          trailing: DropdownButton<Language>(
+            value: Provider.of<Settings>(context).language,
+            items: [
+              DropdownMenuItem(child: Text('한국어'), value: Language.korean),
+              DropdownMenuItem(child: Text('English'), value: Language.english),
+            ],
+            onChanged: (value) {
+              setState(() {
+                Provider.of<Settings>(context, listen: false).language = value!;
+              });
+            },
+          ),
+        ),
+      ],
     );
   }
 }
@@ -106,6 +106,7 @@ class Settings extends ChangeNotifier {
     notifyListeners();
     _saveSettingsToSharedPrefs();
   }
+
   Language get language => _language;
   set language(Language value) {
     _language = value;
@@ -133,4 +134,4 @@ class Settings extends ChangeNotifier {
   }
 }
 
-
+enum Language { korean, english }
