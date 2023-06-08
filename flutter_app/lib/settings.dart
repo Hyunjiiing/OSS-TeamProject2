f11006e6e2b2120e21978ba260b5897faac0d477
Hyunjiiing <h.jilluminate@gmail.com>
2023-06-09 03:34:44 +0900

    설정 화면 SettingScreenState 추가

--- flutter_app/lib/settings.dart
+++ flutter_app/lib/settings.dart
@@ -32,3 +32,10 @@ class SettingScreen extends StatefulWidget {
 @override
 _SettingScreenState createState() => _SettingScreenState();
 }
+
+class _SettingScreenState extends State<SettingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+        children: [
+        ListTile(
