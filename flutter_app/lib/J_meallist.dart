834ea58f77e6015f4f9b8bceaef73f511f7b75a2
KangJiUng <woong236406@gmail.com>
2023-06-10 21:34:00 +0900

    수정 기능 제외

--- flutter_app/lib/J_meallist.dart
+++ flutter_app/lib/J_meallist.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class MealList extends StatelessWidget {
+class MealList extends StatefulWidget {
+  @override
+  _MealListState createState() => _MealListState();
+}
+
+class _MealListState extends State<MealList> {
+  late Stream<QuerySnapshot> _stream;
+  TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _stream = FirebaseFirestore.instance.collection('meal_record').orderBy('날짜').snapshots();
+  }
+
+  void _onSearchChanged(String value) {
+    setState(() {
+      _stream = FirebaseFirestore.instance
+          .collection('meal_record')
+          .where('음식', isEqualTo: value)
+          .orderBy('날짜')
+          .snapshots();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,18 +45,27 @@ class MealList extends StatelessWidget {
           prefixStyle: TextStyle(color: Colors.grey),
         ),
       ),
-      home: HomePage(),
+      home: HomePage(
+        onSearchChanged: _onSearchChanged,
+      ),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
+  final Function(String) onSearchChanged;
+
+  const HomePage({
+    required this.onSearchChanged,
+  });
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late List<DocumentSnapshot> _mealRecords = [];
   late Stream<QuerySnapshot> _stream;
   TextEditingController _searchController = TextEditingController();
 
@@ -40,6 +73,11 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     _stream = firestore.collection('식단').orderBy('날짜').snapshots();
+    _searchController.addListener(_onSearchControllerChanged);
+  }
+
+  void _onSearchControllerChanged() {
+    widget.onSearchChanged(_searchController.text);
   }
 
   @override
@@ -62,27 +100,24 @@ class _HomePageState extends State<HomePage> {
               controller: _searchController,
               cursorColor: const Color(0xffFF923F),
               decoration: InputDecoration(
-                labelText: '음식 이름 검색',
+                labelText: '검색',
                 prefixIcon: Icon(
                   Icons.search,
                   color: Color(0xffFF923F),
-                ),                border: OutlineInputBorder(
-                borderSide: BorderSide(color: const Color(0xffFF923F)),
-              ),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: const Color(0xffFF923F)),
+                ),
                 enabledBorder: UnderlineInputBorder(
                   borderSide: BorderSide(color: Colors.grey),
                 ),
               ),
-              onChanged: (value) {
-                _onSearchChanged(value);
-              },
             ),
           ),
           Expanded(
             child: StreamBuilder<QuerySnapshot>(
               stream: _stream,
-              builder: (BuildContext context,
-                  AsyncSnapshot<QuerySnapshot> snapshot) {
+              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
                 if (snapshot.hasError) {
                   return Text('데이터 가져오기에 오류가 발생했습니다.');
                 }
@@ -91,11 +126,13 @@ class _HomePageState extends State<HomePage> {
                   return CircularProgressIndicator();
                 }
 
-                return ListView(
-                  children:
-                  snapshot.data!.docs.map((DocumentSnapshot document) {
-                    Map<String, dynamic> data =
-                    document.data() as Map<String, dynamic>;
+                _mealRecords = snapshot.data!.docs;
+
+                return ListView.builder(
+                  itemCount: _mealRecords.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    DocumentSnapshot document = _mealRecords[index];
+                    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
                     final documentId = document.id;
 
                     return Dismissible(
@@ -111,48 +148,27 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                       onDismissed: (direction) {
-                        firestore.collection('식단').doc(documentId).delete();
+                        FirebaseFirestore.instance.collection('meal_record').doc(documentId).delete();
                       },
                       child: ListTile(
                         title: Text(data['음식']),
                         subtitle: Text(data['설명']),
                         trailing: Icon(Icons.edit),
-                        onTap: () {
-                          // 항목 수정 처리
-                          // 여기에 수정할 수 있는 화면으로 이동
-                        },
                       ),
                     );
-                  }).toList(),
+                  },
                 );
               },
             ),
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print("hi");
-          /*
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => NewDietRecordScreen(), //화면 연결에서 수정
-            ),
-          );*/
-        },
-        child: Icon(Icons.add),
-      ),
     );
   }
+}
 
-  void _onSearchChanged(String value) {
-    setState(() {
-      _stream = firestore
-          .collection('식단')
-          .where('음식', isEqualTo: value)
-          .orderBy('날짜')
-          .snapshots();
-    });
-  }
+void main() {
+  runApp(MaterialApp(
+    home: MealList(),
+  ));
 }
