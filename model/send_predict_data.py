21e6902d61c5b1e1dd38653fb978efc13af5b43a
Hyunjiiing <h.jilluminate@gmail.com>
2023-06-10 10:39:43 +0900

    python firebase 연결

--- model/send_predict_data.py
+++ model/send_predict_data.py
@@ -2,3 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore, messaging, auth
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+
+cred = credentials.Certificate("./opensource2-4d4a1-firebase-adminsdk-c1i8e-bd7a8d9ab8.json")
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
