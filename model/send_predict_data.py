92c51e925393a0522ed69959ba9237271ad74c22
Hyunjiiing <h.jilluminate@gmail.com>
2023-06-10 10:40:59 +0900

    예측모델 input 데이터

--- model/send_predict_data.py
+++ model/send_predict_data.py
@@ -6,3 +6,10 @@ from sklearn.ensemble import RandomForestClassifier
 cred = credentials.Certificate("./opensource2-4d4a1-firebase-adminsdk-c1i8e-bd7a8d9ab8.json")
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
+
+def predict_voracity(doc) -> int:
+    data_df = pd.read_csv('./data.csv')
+
+    train_X = data_df.drop(['DATE','voracity'], axis=1)
+    train_Y = data_df['voracity']
+    doc_df = pd.DataFrame(doc, index=[0], columns=train_X.columns)
